370c4f72417dcb7309189a1c6c1ddf637e6252f2
Max Kottong <max.kottong@gmail.com>
2024-11-01 00:27:26 -0400

    Folder setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace WattWatch.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
